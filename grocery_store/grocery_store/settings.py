475d91384ab71197fc42b928bc8fa23aad60dca2
nareshpogula <pogula.naresh9@gmail.com>
2024-12-24 12:32:58 +0530

    user login implemented

--- grocery_store/grocery_store/settings.py
+++ grocery_store/grocery_store/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'products',
     'orders',
+    'users',
     'corsheaders'
     
 ]
@@ -134,3 +135,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
